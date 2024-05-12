97285194953963a00cc02315afe0cfc56f0e0a40
Anthony Wiggins <thewiggins21@gmail.com>
2024-05-12 02:07:11 -0400

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+-- Create the database
+DROP DATABASE IF EXISTS techblog_db;
+
+-- Create the database
+CREATE DATABASE techblog_db;
---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+// Description: Helper functions for the application.
+module.exports = {
+    // Function to get the current date
+    formatDate: function (date) {
+        return date.toLocaleDateString();
+    }
+};
