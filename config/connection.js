c7f08d88607f546c5961187dcb2e13247bb3ab14
Anthony Wiggins <thewiggins21@gmail.com>
2024-05-12 14:49:54 -0400

    conn.js edit JAWSDB to deploy app on Heroku

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// sequelize connection to database
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// create connection to our database, pass in your MySQL information for username and password
+let sequelize;
+
+// environment variable for JawsDB that holds the URL to the database
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+// export the connection
+module.exports = sequelize;
