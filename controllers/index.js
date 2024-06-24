cd73eed60af5d6d295bc194663fa32d21d7948a0
Anthony Wiggins <thewiggins21@gmail.com>
2024-06-24 18:00:01 -0400

    testing

--- controllers/index.js
+++ controllers/index.js
@@ -6,11 +6,19 @@ const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api/');
 const dashRoutes = require('./dashRoutes');
 
+router.use('/', (req,res) => {
+
+
+    res.send('ehllo');
+});
+
+/*
 // Define routes
 // Mount the imported routes on their respective endpoints
 router.use('/', homeRoutes); // Mount the homeRoutes on the root endpoint
 router.use('/dashboard', dashRoutes); // Mount the dashRoutes on the /dashboard endpoint
 router.use('/api', apiRoutes); // Mount the apiRoutes on the /api endpoint
+*/
 
 // Export the router with all the routes mounted on it
 module.exports = router;
